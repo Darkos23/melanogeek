b4833b7c52c77087885ecb1131c7f3b06e9fec11
K.L <sarribou259@gmail.com>
2026-04-15 09:41:15 +0000

    Fix Communauté footer link to community route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -977,7 +977,7 @@
     <ul class="lp-footer-links">
         <li><a href="{{ route('blog.index') }}">Blog</a></li>
         <li><a href="{{ route('forum.index') }}">Forum</a></li>
-        <li><a href="{{ route('home') }}">Communauté</a></li>
+        <li><a href="{{ route('community') }}">Communauté</a></li>
         <li><a href="{{ route('about') }}">À propos</a></li>
         <li><a href="#">Mentions légales</a></li>
         <li><a href="#">Contact</a></li>
